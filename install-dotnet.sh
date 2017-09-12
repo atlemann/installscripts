e74e1349cacfc95a59c864a192f61ed5a6c49610
Atle Rudshaug <atle.rudshaug@gmail.com>
2017-09-12 22:49:46 +0200

    Remove echos

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo Installing dotnet core 2.0.0
-
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'

2a06df3b5f51d5ebfb610bbc62b9c76cc091c079
Atle Rudshaug <atle.rudshaug@gmail.com>
2017-09-12 21:34:59 +0200

    Added install scripts for mono and dotnet

---
+++ install-mono.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Installing Mono and fsharp
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $ 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee $ /etc/apt/sources.list.d/mono-official.list
+apt-get update
+apt-get install fsharp

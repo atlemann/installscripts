0eeb5d5c19d3edfc86775c3569f266ccdcd63e54
Atle Rudshaug <atle.rudshaug@gmail.com>
2017-10-23 22:53:36 +0200

    Installing Ionide and C# extensions with VSCode

--- install-vscode.sh
+++ install-vscode.sh
@@ -2,3 +2,8 @@
 wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
 sudo dpkg -i vscode.deb
 sudo apt install -f
+code --install-extension Ionide.ionide-fsharp
+code --install-extension Ionide.ionide-Paket
+code --install-extension Ionide.ionide-FAKE
+code --install-extension ms-vscode.csharp
+

f865b9e8991ec3590093d2d79524ef55f6e5874d
Atle Rudshaug <atle.rudshaug@gmail.com>
2017-10-25 23:19:24 +0200

    Added git cm alias which adds all unstaged files and expects a commit message.

--- setup-git.sh
+++ setup-git.sh
@@ -3,6 +3,7 @@
 git config --global alias.co checkout
 git config --global alias.cob "checkout -b"
 git config --global alias.ci commit
+git config --global alias.cm '!git add -A && git commit -m'
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.bra "branch -a"

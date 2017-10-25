365343edd7c78ecc88d6f2316b719b2000bcafc7
Atle Rudshaug <atle.rudshaug@gmail.com>
2017-10-25 20:14:07 +0200

    Added configuring of git branch info in the command line

--- setup-git.sh
+++ setup-git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Credits: https://haacked.com/archive/2014/07/28/github-flow-aliases/
+# Git aliases. https://haacked.com/archive/2014/07/28/github-flow-aliases/
 git config --global alias.co checkout
 git config --global alias.cob "checkout -b"
 git config --global alias.ci commit
@@ -9,3 +9,14 @@ git config --global alias.bra "branch -a"
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
 git config --global alias.up "up = !git pull --rebase --prune $@ && git submodule update --init --recursive"
 git config --global alias.ec "config --global -e"
+
+# Git branch info in terminal prompt
+curl -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+
+cat >> ~/.git-prompt.sh <<EOF
+# Git branch info in terminal prompt
+. ~/.git-prompt.sh
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWCOLORHINTS=true
+export PROMPT_COMMAND='__git_ps1 "\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]" "\\\$ "'
+EOF

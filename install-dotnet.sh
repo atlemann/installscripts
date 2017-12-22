6f62caa95e58c7eee6b2a95d2e3d744374e661f1
Atle Rudshaug <atle.rudshaug@gmail.com>
2017-12-22 22:34:43 +0100

    Bump dotnet sdk to v2.1.3
    dotnet restore should now support semver versioned nuget packages and `FrameworkPathOverride` environment variable for invoking Mono from dotnet cli for .net full framework targets.

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -3,4 +3,4 @@
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-apt-get update && apt-get -y install dotnet-sdk-2.0.0
+apt-get update && apt-get -y install dotnet-sdk-2.1.3

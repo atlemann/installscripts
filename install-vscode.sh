a5bab2ee09ad0eee48bf618ff9e26327a6623f76
Atle Rudshaug <atle.rudshaug@gmail.com>
2018-01-13 23:09:38 +0100

    Install using apt-get
    Add vscode package source manually and install using apt-get

--- install-vscode.sh
+++ install-vscode.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
-sudo dpkg -i vscode.deb
-sudo apt install -f
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install -y code
 code --install-extension Ionide.ionide-fsharp
 code --install-extension Ionide.ionide-Paket
 code --install-extension Ionide.ionide-FAKE

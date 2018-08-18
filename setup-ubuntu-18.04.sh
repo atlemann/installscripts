0f0a23297a4209325d547a6ffd46b8dd0129bb14
Atle <atle.rudshaug@gmail.com>
2018-08-18 21:48:19 +0200

    Added script to set up F# dotnet core on Ubuntu 18.04

---
+++ setup-ubuntu-18.04.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt-get -y install curl
+
+# dotnet core sdk
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+# VSCode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get -y install apt-transport-https
+sudo apt-get update
+sudo apt-get -y install dotnet-sdk-2.1 code fonts-firacode
+
+code --install-extension Ionide.ionide-fsharp
+code --install-extension Ionide.ionide-Paket
+code --install-extension Ionide.ionide-FAKE
+code --install-extension ms-vscode.csharp
+
+echo "Creating VSCode settings file $file."
+
+folder="$HOME/.config/Code/User"
+file="$folder/settings.json"
+
+mkdir -p $folder
+cat >> $file <<EOF
+{
+    "editor.fontLigatures": true,
+    "editor.fontFamily": "Fira Code",
+    "editor.fontSize": 14,
+    "FSharp.fsacRuntime": "netcore"
+}
+EOF
+
+./setup-git.sh
